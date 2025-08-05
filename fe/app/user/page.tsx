00966b27b729b458f379729f949a893b161e6092
Andhika-GIT <andhikapr081@gmail.com>
2025-08-05 20:26:46 +0700

    feat: add dropzone component and create new component for upload file

--- fe/app/user/page.tsx
+++ fe/app/user/page.tsx
@@ -2,10 +2,12 @@ import { DataTable } from "@/components/data-table";
 
 import user from "./user.json";
 import { columns } from "./column";
+import { UploadSection } from "@/components/molecules";
 
 export default function Page() {
   return (
     <>
+      <UploadSection />
       <DataTable columns={columns} data={user} />
     </>
   );
