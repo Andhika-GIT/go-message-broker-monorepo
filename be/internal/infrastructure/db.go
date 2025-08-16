5c1439be03c1c311b953d44fe658fa6edd37c5d4
Andhika-GIT <andhikapr081@gmail.com>
2025-08-16 20:05:53 +0700

    feat: setup db and viper ( for env )

--- be/internal/infrastructure/db.go
+++ be/internal/infrastructure/db.go
@@ -1,1 +1,27 @@
 package infrastructure
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDatabase(viper *viper.Viper) *gorm.DB {
+	DB_HOST := viper.GetString("DB_HOST")
+	DB_NAME := viper.GetString("DB_NAME")
+	DB_PORT := viper.GetString("DB_PORT")
+	DB_USERNAME := viper.GetString("DB_USERNAME")
+	DB_PASSWORD := viper.GetString("DB_PASSWORD")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(fmt.Errorf("fatal error connecting to database: %w", err))
+	}
+
+	return db
+
+}
