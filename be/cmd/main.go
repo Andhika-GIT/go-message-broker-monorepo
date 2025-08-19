b36eddb0fa7a9dab70dbaf7876d879d1fba36ab7
Andhika-GIT <andhikapr081@gmail.com>
2025-08-19 20:56:57 +0700

    feat: install chi router and setup initApp

--- be/cmd/main.go
+++ be/cmd/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/Andhika-GIT/go-message-broker-monorepo/internal/infrastructure"
+)
+
+func main() {
+	r := infrastructure.InitApp()
+
+	http.ListenAndServe(":3000", r)
+}
--- be/internal/infrastructure/app.go
+++ be/internal/infrastructure/app.go
@@ -1,1 +1,12 @@
 package infrastructure
+
+import "github.com/go-chi/chi/v5"
+
+func InitApp() *chi.Mux {
+	r := chi.NewRouter()
+
+	v := NewViper()
+	NewRabbitMqProducer(v)
+
+	return r
+}
