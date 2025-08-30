42730ee2046bd70bb3e46ac86b9fb7c871c5d607
Andhika-GIT <andhikapr081@gmail.com>
2025-08-30 13:12:45 +0700

    feat(be): improve rabbitmq reusable function

--- be/internal/shared/mq.go
+++ be/internal/shared/mq.go
@@ -31,3 +31,52 @@ func NewRabbitMqProducer(viper *viper.Viper) (*RabbitMqProducer, error) {
 		Channel: ch,
 	}, nil
 }
+
+func (c *RabbitMqProducer) Close() {
+	if c.Channel != nil {
+		_ = c.Channel.Close()
+	}
+
+	if c.Conn != nil {
+		_ = c.Conn.Close()
+	}
+}
+
+func (c *RabbitMqProducer) QueueDeclare() (amqp091.Queue, error) {
+	q, err := c.Channel.QueueDeclare(
+		"test_queue", true, false, false, false,
+		amqp091.Table{
+			"x-queue-type": "quorum",
+		},
+	)
+
+	if err != nil {
+		return q, fmt.Errorf("failed to decleare queue : %v", err.Error())
+	}
+
+	err = c.Channel.QueueBind(
+		q.Name, "test", "go-exchange", false, nil,
+	)
+
+	if err != nil {
+		return q, fmt.Errorf("failed to bind queue : %v", err.Error())
+	}
+
+	return q, nil
+}
+
+func (c *RabbitMqProducer) Publish() error {
+	err := c.Channel.Publish(
+		"go-exchange", "test", false, false,
+		amqp091.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("ini dari handler controller"),
+		},
+	)
+
+	if err != nil {
+		return fmt.Errorf("failed to publish message: %v", err)
+	}
+
+	return nil
+}
