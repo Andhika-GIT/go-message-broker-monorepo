3f23fedb61e2fa6800ae228f849d407ee3a34440
Andhika-GIT <andhikapr081@gmail.com>
2025-08-30 13:42:42 +0700

    feat: setup user module

--- be/internal/user/config.go
+++ be/internal/user/config.go
@@ -1,1 +1,11 @@
 package user
+
+import (
+	"github.com/Andhika-GIT/go-message-broker-monorepo/internal/shared"
+	"github.com/go-chi/chi/v5"
+)
+
+func NewUserModule(r chi.Router, rmq *shared.RabbitMqProducer) {
+	handler := NewUserHandler(rmq)
+	NewUserRoutes(r, handler)
+}
--- be/internal/user/handler.go
+++ be/internal/user/handler.go
@@ -1,1 +1,32 @@
 package user
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Andhika-GIT/go-message-broker-monorepo/internal/shared"
+)
+
+type UserHandler struct {
+	rmq *shared.RabbitMqProducer
+}
+
+func NewUserHandler(rmq *shared.RabbitMqProducer) *UserHandler {
+	return &UserHandler{
+		rmq: rmq,
+	}
+}
+
+func (h *UserHandler) UploadUser(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 << 20)
+
+	file, header, err := r.FormFile("file")
+
+	if err != nil {
+		log.Printf("Failed to read file : %v", err)
+	}
+
+	defer file.Close()
+
+	log.Printf("uploaded file : %s (%d bytes)", header.Filename, header.Size)
+}
