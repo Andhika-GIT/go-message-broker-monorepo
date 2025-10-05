77d25be52ff952c291c2ef57c44fa4f4b15595d7
Andhika-GIT <andhikapr081@gmail.com>
2025-10-05 19:56:56 +0700

    feat: delete password_hash field from users table

--- upload-worker/internal/user/entity.go
+++ upload-worker/internal/user/entity.go
@@ -5,7 +5,6 @@ import "time"
 type User struct {
 	ID          int64     `json:"id" gorm:"primary_key;column:id"`
 	Name        string    `json:"name" gorm:"column:name"`
-	Password    string    `gorm:"column:password; not null"`
 	Email       string    `json:"email" gorm:"column:email"`
 	PhoneNumber string    `json:"phone_number" gorm:"column:phone_number"`
 	CreatedAt   time.Time `json:"created_at" gorm:"column:created_at;autoCreateTime"`
