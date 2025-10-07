20112bad367095ca19cc91db4ff03593b689bad9
Andhika-GIT <andhikapr081@gmail.com>
2025-10-07 13:12:34 +0700

    feat: setup order direct worker and module

--- upload-worker/internal/order/config.go
+++ upload-worker/internal/order/config.go
@@ -1,1 +1,15 @@
 package order
+
+import (
+	"github.com/Andhika-GIT/go-message-broker-monorepo/internal/shared"
+	"gorm.io/gorm"
+)
+
+func NewOrderModule(rmq *shared.RabbitMqConsumer, DB *gorm.DB) {
+	orderUseCase := NewOrderUseCase(&OrderRepository{}, DB)
+
+	directUC := NewOrderDirectWorker(rmq, orderUseCase)
+
+	go directUC.Start()
+
+}
--- upload-worker/internal/order/direct-worker.go
+++ upload-worker/internal/order/direct-worker.go
@@ -1,1 +1,17 @@
 package order
+
+import "github.com/Andhika-GIT/go-message-broker-monorepo/internal/shared"
+
+type OrderDirectWorker struct {
+	Rmq     *shared.RabbitMqConsumer
+	UseCase *OrderUseCase
+}
+
+func NewOrderDirectWorker(Rmq *shared.RabbitMqConsumer, UseCase *OrderUseCase) *OrderDirectWorker {
+	return &OrderDirectWorker{
+		Rmq:     Rmq,
+		UseCase: UseCase,
+	}
+}
+
+func (w *OrderDirectWorker) Start() {}
