47835bd57ef38a21658896dec928602646b69ca9
Andhika-GIT <andhikapr081@gmail.com>
2025-10-07 12:06:27 +0700

    feat: setup entity,model and usecase for order service

--- upload-worker/internal/order/repository.go
+++ upload-worker/internal/order/repository.go
@@ -1,1 +1,19 @@
 package order
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+type OrderRepository struct{}
+
+func (r *OrderRepository) Create(c context.Context, tx *gorm.DB, order *Order) error {
+	err := tx.Create(&order).Error
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- upload-worker/internal/order/usecase.go
+++ upload-worker/internal/order/usecase.go
@@ -1,1 +1,15 @@
 package order
+
+import "gorm.io/gorm"
+
+type OrderUseCase struct {
+	Repository *OrderRepository
+	DB         *gorm.DB
+}
+
+func NewOrderUseCase(Repository *OrderRepository, DB *gorm.DB) *OrderUseCase {
+	return &OrderUseCase{
+		Repository: Repository,
+		DB:         DB,
+	}
+}
