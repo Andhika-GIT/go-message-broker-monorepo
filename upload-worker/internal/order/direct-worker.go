3a242d1ee2950794887920368f130f7c8d5c664b
Andhika-GIT <andhikapr081@gmail.com>
2025-10-07 13:45:29 +0700

    feat: setup order direct worker function and queue constant

--- upload-worker/internal/order/direct-worker.go
+++ upload-worker/internal/order/direct-worker.go
@@ -1,6 +1,11 @@
 package order
 
-import "github.com/Andhika-GIT/go-message-broker-monorepo/internal/shared"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/Andhika-GIT/go-message-broker-monorepo/internal/shared"
+)
 
 type OrderDirectWorker struct {
 	Rmq     *shared.RabbitMqConsumer
@@ -14,4 +19,29 @@ func NewOrderDirectWorker(Rmq *shared.RabbitMqConsumer, UseCase *OrderUseCase) *
 	}
 }
 
-func (w *OrderDirectWorker) Start() {}
+func (w *OrderDirectWorker) Start() {
+	defer w.Rmq.Close()
+
+	// ch := make(chan OrderImport)
+	// c := context.Background()
+
+	msgs, err := w.Rmq.Consume(shared.QueueOrderDirectImport)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	var orders []OrderImport
+	for msg := range msgs {
+		err := json.Unmarshal(msg.Body, &orders)
+
+		if err != nil {
+			log.Panicln(err.Error())
+			continue
+		}
+
+		for _, order := range orders {
+			log.Printf("Name: %s, Email: %s, Phone: %s\n", order.Email, order.ProductName, order.Quantity)
+		}
+	}
+}
--- upload-worker/internal/shared/constant.go
+++ upload-worker/internal/shared/constant.go
@@ -7,18 +7,20 @@ const (
 
 // Routing Keys
 const (
-	RoutingKeyUserDirectImport = "user.import.direct"
-	RoutingKeyUserSftpImport   = "user.import.sftp"
-	RoutingKeyUserExport       = "user.export"
-	RoutingKeyOrderImport      = "order.import"
-	RoutingKeyOrderExport      = "order.export"
+	RoutingKeyUserDirectImport  = "user.import.direct"
+	RoutingKeyUserSftpImport    = "user.import.sftp"
+	RoutingKeyUserExport        = "user.export"
+	RoutingKeyOrderDirectImport = "order.import.direct"
+	RoutingKeyOrderImport       = "order.import"
+	RoutingKeyOrderExport       = "order.export"
 )
 
 // Queue Names
 const (
-	QueueUserDirectImport = "user.import.direct.q"
-	QueueUserSftpImport   = "user.import.sftp.q"
-	QueueUserExport       = "user.export.q"
-	QueueOrderImport      = "order.import.q"
-	QueueOrderExport      = "order.export.q"
+	QueueUserDirectImport  = "user.import.direct.q"
+	QueueUserSftpImport    = "user.import.sftp.q"
+	QueueUserExport        = "user.export.q"
+	QueueOrderDirectImport = "order.import.direct.q"
+	QueueOrderImport       = "order.import.q"
+	QueueOrderExport       = "order.export.q"
 )
