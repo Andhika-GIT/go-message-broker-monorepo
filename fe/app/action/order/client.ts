30d3c8d401b54547a37b3c827f782e15301fb936
Andhika-GIT <andhikapr081@gmail.com>
2025-10-29 13:35:50 +0700

    feat[FE]: upload order section and new function

--- fe/app/action/order/client.ts
+++ fe/app/action/order/client.ts
@@ -6,6 +6,25 @@ import { Error, Paginate } from "@/lib/types";
 
 type FetchResult = Paginate<Order[]>
 
+export const UploadOrderExcel = async (file: File): Promise<string | undefined> => {
+  const BASE_URL = `${SERVER_BASE_URL_FOR_CLIENT}/order/upload`;
+
+  try {
+
+    const formData = new FormData()
+    formData.append("file", file)
+
+    const response = await fetch(BASE_URL, {
+      method: "POST",
+      body:formData,
+    });
+
+    return await handleFetchResponse(response);
+  } catch (e) {
+    throw e as Error;
+  }
+};
+
 export const getAllOrders = async (page: number, pageSize: number): Promise<FetchResult | undefined> => {
   const BASE_URL = `${SERVER_BASE_URL_FOR_CLIENT}/order?page=${page}&per_page=${pageSize}`;
 
--- fe/app/order/page.tsx
+++ fe/app/order/page.tsx
@@ -2,12 +2,12 @@ import { DataTable } from "@/components/data-table";
 
 import { columns } from "./column";
 import { UploadSection } from "@/components/molecules";
-import { getAllOrders } from "../action/order";
+import { getAllOrders, UploadOrderExcel } from "../action/order";
 
 export default async function Page() {
   return (
     <>
-      {/* <UploadSection uploadFn={UploadUserExcel} /> */}
+      <UploadSection uploadFn={UploadOrderExcel} />
       <DataTable columns={columns} fetchFunction={getAllOrders}/>
     </>
   );
